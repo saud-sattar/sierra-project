e5f12b47feb60870ffcadab2c7221c5284fe0a69
Saud Sattar <83525131+saud-sattar@users.noreply.github.com>
2024-12-25 01:00:35 +1100

    test 0.1 finished at step 76
    using this test branch proved to be very helpful as it helped me realise I didn't do anything wrong but I was just not opening a file

--- sierra.c
+++ sierra.c
@@ -42,6 +42,7 @@ struct editorConfig
 {
    int cx, cy;
    int rowoff;
+   int coloff;
    int screenrows;
    int screencols;
    int numrows;
@@ -280,6 +281,14 @@ void editorScroll()
    {
       E.rowoff = E.cy - E.screenrows + 1;
    }
+   if (E.cx < E.coloff)
+   {
+      E.coloff = E.cx;
+   }
+   if (E.cx >= E.coloff + E.screencols)
+   {
+      E.coloff = E.cx - E.screencols + 1;
+   }
 }
 
 void editorDrawRows(struct abuf *ab)
@@ -314,10 +323,12 @@ void editorDrawRows(struct abuf *ab)
       }
       else
       {
-         int len = E.row[filerow].size;
+         int len = E.row[filerow].size - E.coloff;
+         if (len < 0)
+            len = 0;
          if (len > E.screencols)
             len = E.screencols;
-         abAppend(ab, E.row[filerow].chars, len);
+         abAppend(ab, &E.row[filerow].chars[E.coloff], len);
       }
 
       abAppend(ab, "\x1b[K", 3);
@@ -338,7 +349,8 @@ void editorRefreshScreen()
    editorDrawRows(&ab);
 
    char buf[32];
-   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+            (E.cx - E.coloff) + 1);
    abAppend(&ab, buf, strlen(buf));
 
    abAppend(&ab, "\x1b[?25h", 6);
@@ -350,6 +362,8 @@ void editorRefreshScreen()
 /*** input ***/
 void editorMoveCursor(int key)
 {
+   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
    switch (key)
    {
    case ARROW_LEFT:
@@ -359,7 +373,7 @@ void editorMoveCursor(int key)
       }
       break;
    case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1)
+      if (row && E.cx < row->size)
       {
          E.cx++;
       }
@@ -421,6 +435,7 @@ void initEditor()
    E.cx = 0;
    E.cy = 0;
    E.rowoff = 0;
+   E.coloff = 0;
    E.numrows = 0;
    E.row = NULL;
 
